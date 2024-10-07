fec7b153c3423d52d9a56af5da888666b6df1d72
Harrynerji <elmarhus99@gmail.com>
2024-10-07 17:24:53 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+
+let number = 5;
+const leftBorderWidth = 1;
+
+console.log();
+
+const persone ="Alex";
+
+
+let und;
+console.log(und);
+
+
+const obj = {
+    name : "John",
+    age : 25,
+    isMarried : false 
+};
+
+let arr = [
+    "Asif","Vasif","Eldar",6
+];
+console.log(arr[1]);
+
+
+// const answer = prompt("How old a u?",20);
+// console.log(answer + 5);
+
+// const answer = [];
+
+// answer [0] = prompt('wht is u name?',"");
+// answer [1] = prompt('wht is u surname?',"");
+// answer [2] = prompt('how old a u?',"");
+
+// console.log(typeof(answer));
+
+// const category = "toys";
+// console.log(`https://someurl.com/${category}`);
+
+// const user = "Elmar";
+
+// alert (`"HI",${user}`)
+
+
+// console.log("arr" + "- object");
+
+// console.log(4 + "5");
+
+// let incr= 10,
+//     decr =10;
+
+
+//     console.log(incr++);
+//     console.log(decr--);
+
+//     console.log(5%2);
+
+//     console.log(2*4 ==="8");
+    
+
+// const isChecked = true,
+//       isClose = false;
+
+//       console.log(isChecked || isClose);
+      
+    
+    
+    
